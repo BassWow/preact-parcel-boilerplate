70efa5eaaa6ec744874d3181dcb9be93cff1d9de
BassWow <bhassan@woolworths.com.au>
2021-04-07 22:56:15 +1000

    adding twin.macro

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import { h, render } from "preact";
 import { Router } from "preact-router";
 import { BeakerIcon } from "@heroicons/react/solid";
+import tw, { styled } from "twin.macro"
 
+//const BigHeading = 
 const Home = () => (
   <div className="container mx-auto flex items-center justify-center h-screen font-mono">
     <div className="mx-auto">
-      <h1 className="text-2xl font-mono subpixl-antialiased font-thin text-center">Welcome to the Parcel | PREACT | TaildwindCSS Boilerplate!</h1>
+      <h1 tw="text-2xl font-mono subpixel-antialiased font-bold text-center text-red-700">Welcome to the Parcel | PREACT | TaildwindCSS Boilerplate!</h1>
       <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4 dark:bg-gray-800 my-10">
         <div className="flex-shrink-0">
-          <BeakerIcon className="h-20 w-20 text-blue-500" />
+          <BeakerIcon className="h-20 w-20 text-red-300" />
         </div>
         <div>
           <h1 className="text-3xl font-medium text-white">Boilerplate</h1>
